3cb1d679b70b7b2085e7b3e205edc462e777345e
parasvcb <parasvcb@gmail.com>
2020-08-26 15:11:34 +0530

    slight updation in pythonjob from hulk

--- python_job.py
+++ python_job.py
@@ -4,15 +4,15 @@ import re
 import os
 import module_md
 import sys
-if len(sys.argv)!=4:
-    print ("please enter correct cmd 1. equilibration till 10 ns (10 conf files should be ready) or production (1 for former 2 for latter) 2: production run in ns (100 for latte 0 for former) 3:DoneProductionTime(if no dcd immediately available then enter time in ns else False)")
+if len(sys.argv)!=5:
+    print ("please enter correct cmd 1. directoryHavingFilestructure 2. equilibration till 10 ns (10 conf files should be ready) or production (1 for former 2 for latter) 3. production run in ns (100 for latte 0 for former) 4. DoneProductionTime(if no dcd immediately available then enter time in ns else False)")
     sys.exit()
-prog,runtype,time,doneTime=sys.argv
+prog,systemDir,runtype,time,doneTime=sys.argv
 doneTime=float(doneTime)
 default_cmd_str = "~/bin/namdcuda2.13 +p10 +idlepoll"
 
 # will run minimization and then 200 ps of temop eqil then 5 200ps each of press equils,
-
+os.chdir(systemDir)
 if int(runtype)==1:
     module_md.minimization(default_cmd_str)
     module_md.temp_equil(default_cmd_str)
